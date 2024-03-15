75467a50eb00a8f4a8069101f50733e01d26cec2
Shihab Hasan <shihabbd018@gmail.com>
2024-03-15 14:03:30 +0600

    modified social links on footer

--- src/components/Social links/SocialLinks.tsx
+++ src/components/Social links/SocialLinks.tsx
@@ -13,8 +13,8 @@ const SocialLinks = () => {
             <span className='text-5xl lg:text-6xl hover:text-gray-200'><FaLinkedin /></span>
             <div>
                 <h3 className=" font-merriweather font-medium ">LinkedIn</h3>
-                <span className=" font-roboto font-[300] text-sm">12k followers</span>
-                <Link to="https://www.linkedin.com/in/atm-mahmud-95570333" className="flex items-center gap-2 mt-5 text-[#C58940] text-lg " target="_blank">Follow<IoIosArrowRoundForward className="text-2xl" /> </Link>
+
+                <Link to="https://www.linkedin.com/in/atm-mahmud-95570333" className="flex items-center gap-2 mt-1 text-[#C58940] text-lg " target="_blank">Follow<IoIosArrowRoundForward className="text-2xl" /> </Link>
             </div>
         </div>
 
@@ -23,8 +23,8 @@ const SocialLinks = () => {
             <span className='text-5xl lg:text-6xl hover:text-gray-200'><FaFacebookSquare /></span>
             <div>
                 <h3 className=" font-merriweather font-medium ">Facebook</h3>
-                <span className=" font-roboto font-[300] text-sm">1k followers</span>
-                <Link to="https://www.facebook.com/founder.atms" className="flex items-center gap-2 mt-5 text-[#C58940] text-lg " target="_blank">Follow<IoIosArrowRoundForward className="text-2xl" /> </Link>
+
+                <Link to="https://www.facebook.com/founder.atms" className="flex items-center gap-2 mt-1 text-[#C58940] text-lg " target="_blank">Follow<IoIosArrowRoundForward className="text-2xl" /> </Link>
             </div>
         </div>
 
@@ -33,8 +33,8 @@ const SocialLinks = () => {
             <span className='text-5xl lg:text-6xl hover:text-gray-200'><FaInstagramSquare /></span>
             <div>
                 <h3 className=" font-merriweather font-medium ">Instagram</h3>
-                <span className=" font-roboto font-[300] text-sm">961 followers</span>
-                <Link to="https://www.instagram.com/atmmahmud/" className="flex items-center gap-2 mt-5 text-[#C58940] text-lg" target="_blank" >Follow <IoIosArrowRoundForward className="text-2xl" /> </Link>
+
+                <Link to="https://www.instagram.com/atmmahmud/" className="flex items-center gap-2 mt-1 text-[#C58940] text-lg" target="_blank" >Follow <IoIosArrowRoundForward className="text-2xl" /> </Link>
             </div>
         </div>
 
@@ -43,8 +43,8 @@ const SocialLinks = () => {
             <span className='text-5xl lg:text-6xl hover:text-gray-200'><FaSquareTwitter /></span>
             <div>
                 <h3 className=" font-merriweather font-medium ">Twitter</h3>
-                <span className=" font-roboto font-[300] text-sm">1k followers</span>
-                <Link to="https://twitter.com/atmmahmud1" className="flex items-center gap-2 mt-5 text-[#C58940] text-lg" target="_blank" >Follow <IoIosArrowRoundForward className="text-2xl" /> </Link>
+
+                <Link to="https://twitter.com/atmmahmud1" className="flex items-center gap-2 mt-1 text-[#C58940] text-lg" target="_blank" >Follow <IoIosArrowRoundForward className="text-2xl" /> </Link>
             </div>
         </div>
 
@@ -53,8 +53,8 @@ const SocialLinks = () => {
             <span className='text-3xl p-[9px] lg:text-4xl hover:bg-gray-200 text-black bg-white rounded-xl h-14'  ><FaQuora /></span>
             <div>
                 <h3 className=" font-merriweather font-medium ">Quora</h3>
-                <span className=" font-roboto font-[300] text-sm">1k followers</span>
-                <Link to="https://bn.quora.com/profile/ATM-Sir-3" className="flex items-center gap-2 mt-5 text-[#C58940] text-lg" target="_blank" >Follow <IoIosArrowRoundForward className="text-2xl" /> </Link>
+
+                <Link to="https://bn.quora.com/profile/ATM-Sir-3" className="flex items-center gap-2 mt-1 text-[#C58940] text-lg" target="_blank" >Follow <IoIosArrowRoundForward className="text-2xl" /> </Link>
             </div>
         </div>
 
